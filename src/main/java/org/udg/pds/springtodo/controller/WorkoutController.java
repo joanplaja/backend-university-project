5c1e4dc5ff6ca0ec955681c677a2f3234d0441ee
pauvilella <pau.vilella.st@gmail.com>
2021-03-30 17:42:36 +0200

    Afegir els endpoints POST /workouts/{wid}/routes i GET /workouts/{wid}/routes. Arreglar alguns bugs.

--- src/main/java/org/udg/pds/springtodo/controller/WorkoutController.java
+++ src/main/java/org/udg/pds/springtodo/controller/WorkoutController.java
@@ -6,11 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.udg.pds.springtodo.controller.exceptions.ControllerException;
-import org.udg.pds.springtodo.entity.IdObject;
-import org.udg.pds.springtodo.entity.Point;
-import org.udg.pds.springtodo.entity.Workout;
-import org.udg.pds.springtodo.entity.Views;
+import org.udg.pds.springtodo.entity.*;
 import org.udg.pds.springtodo.serializer.JsonDateDeserializer;
+import org.udg.pds.springtodo.service.PointService;
+import org.udg.pds.springtodo.service.RouteService;
 import org.udg.pds.springtodo.service.WorkoutService;
 
 
@@ -21,6 +20,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.Map;
 
 @RequestMapping(path="/workouts")
 @RestController
@@ -28,6 +28,12 @@ public class WorkoutController extends BaseController {
 
     @Autowired
     WorkoutService workoutService;
+
+    @Autowired
+    RouteService routeService;
+
+    @Autowired
+    PointService pointService;
 
     @GetMapping(path="/{id}")
     public Workout getWorkout(HttpSession session,
@@ -53,6 +59,24 @@ public class WorkoutController extends BaseController {
 
         return workoutService.addWorkout(workout.type, userId, workout.dateCreated);
     }
+
+    @GetMapping(path = "/{id}/routes")
+    @JsonView(Views.Private.class)
+    public Route getWorkoutRoute(HttpSession session, @PathVariable("id") Long workoutId) {
+        Long userId = getLoggedUser(session);
+
+        return routeService.getRoute(workoutId);
+    }
+
+    @PostMapping(path = "/{id}/routes", consumes = "application/json")
+    public IdObject addRoute(HttpSession session, @Valid @RequestBody R_Route route, @PathVariable("id") Long workoutId) {
+
+        Long userId = getLoggedUser(session);
+
+        return routeService.addRoute(userId, workoutId, route.initialLatitude, route.initialLongitude);
+    }
+
+
 
     @DeleteMapping(path="/{id}")
     public String deleteWorkout(HttpSession session,
@@ -80,12 +104,12 @@ public class WorkoutController extends BaseController {
 
     }
 
-    static class R_Add_Points_To_Route {
+    static class R_Points {
         @NotNull
-        public Long workoutId;
+        public Long routeId;
 
         @NotNull
-        public ArrayList<Point> points;
+        public ArrayList<Map<Double, Double>> points;
 
     }
 
